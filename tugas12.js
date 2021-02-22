b5ea25a31c573b3e0b4f4eece51ef7757ccf2e6c
heriksuseno <heriksuseno21@mail.com>
2021-02-22 22:20:49 +0700

    Tugas 12 JS untuk NIOMIC

---
+++ tugas12.js
@@ -0,0 +1,9 @@
+function tanaman() {
+  var buah  = ['Pisang','Jeruk','Apel','Mangga']
+  console.log(buah)
+  buah.pop()
+  console.log(buah)
+  buah.shift()
+  return buah
+  }
+console.log(tanaman())
